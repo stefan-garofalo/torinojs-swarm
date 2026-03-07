0ce1797bf66ee1f5426158d07b689d277c673a8c
Stefan Garofalo <113015602+stefan-garofalo@users.noreply.github.com>
2026-03-07 04:17:43 +0100

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@my-better-t-app/db";
+import * as schema from "@my-better-t-app/db/schema/auth";
+import { env } from "@my-better-t-app/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
